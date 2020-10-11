d265f13dac337c397084c656ee14ca848214d40c
Roger Wu <weayzehoa@gmail.com>
2020-10-11 10:58:30 +0800

    修改 config/app.php

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
     composer require laravel/ui "^1.0" --dev
     php artisan ui vue --auth
     npm install && npm run dev
+2. Install Laravel/Socailtie
+3. 修改 config/app.php
+    a. providers區段加入
+        Laravel\Socialite\SocialiteServiceProvider::class,
+    b. aliases區段加入
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Laravel\Socialite\SocialiteServiceProvider::class,
     ],
 
     /*
@@ -225,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
     ],
 
 ];
